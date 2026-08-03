809ed21b640f7bd745fcb09c0bc9ea978db917ec
The Cook Rat <ijrambux@gmail.com>
2026-08-03 11:49:15 +0100

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="ar" dir="rtl">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300;400;500;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="font-tajawal bg-darker text-white">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
